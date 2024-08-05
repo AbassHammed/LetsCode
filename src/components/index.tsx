d5e585cebfe9cc8521be6c13e5322b5f10a33b58
Abass Hammed <abasshammedola@gmail.com>
2024-08-05 13:29:44 +0200

    Finessed the component folder

---
+++ src/components/index.tsx
@@ -0,0 +1,30 @@
+export { Button, buttonVariants } from './shared/button';
+export { default as Icons } from './shared/icons';
+export { Input } from './shared/input';
+export { Label } from './shared/label';
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor } from './shared/popover';
+export {
+  type ToastProps,
+  type ToastActionElement,
+  ToastProvider,
+  ToastViewport,
+  Toast,
+  ToastTitle,
+  ToastDescription,
+  ToastClose,
+  ToastAction,
+  useToast,
+  toast,
+  Toaster,
+} from './shared/toast';
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from './shared/form';
+export { Switch } from './shared/switch';
