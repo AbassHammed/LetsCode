cfc8e83f5e5495641aaa68f87048429d3f200b44
Abass Hammed <abasshammedola@gmail.com>
2024-08-05 19:58:18 +0200

    Done with the forgot-password page

---
+++ src/app/auth/password/forgot-password/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+import { siteConfig } from '@config/site';
+import { ForgotPassword } from '@pages';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Forgot Password',
+    template: `%s | ${siteConfig.url}`,
+  },
+};
+
+export default function ForgotPasswordPage() {
+  return <ForgotPassword />;
+}
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,2 +1,3 @@
 export { default as Login } from './login';
 export { default as SignUpForm } from './signup';
+export { default as ForgotPassword } from './forgot-password';
