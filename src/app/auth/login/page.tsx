be55bab05b6e49f1d7193be8ea633ed11886b73b
Abass Hammed <abasshammedola@gmail.com>
2024-08-05 17:29:20 +0200

    Setting the login page

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next';
+
+import { siteConfig } from '@config/site';
+import { Login } from '@pages';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Login',
+    template: `%s | ${siteConfig.url}`,
+  },
+};
+
+export default function LoginPage() {
+  return <Login />;
+}
