43d2f14b04f481ec2a1abfb75619aacdb001214d
Abass Hammed <abasshammedola@gmail.com>
2024-08-05 17:20:09 +0200

    Added the login component

---
+++ src/components/pages/login.tsx
@@ -0,0 +1,175 @@
+/* eslint-disable quotes */
+'use client';
+
+import React, { useState } from 'react';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  Icons,
+  Input,
+  useToast,
+} from '@components';
+import { auth } from '@firebase/firebase';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoginInputs } from '@types';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const passwordSchema = z
+  .string()
+  .min(8, { message: 'Your password has at least 8 characters.' })
+  .max(20, { message: "Your password can't have more than 20 characters" })
+  .refine(password => /[A-Z]/.test(password), {
+    message: 'Your password definitely contains at least an uppercase character.',
+  })
+  .refine(password => /[a-z]/.test(password), {
+    message: 'Your password also contains at least a lowercase character.',
+  })
+  .refine(password => /[0-9]/.test(password), {
+    message: 'There must have been a numeric character in your password.',
+  })
+  .refine(password => /[!@#$%^&*]/.test(password), {
+    message: "You can't have a password without at least a special character",
+  });
+
+const loginFormSchema = z.object({
+  email: z.string({ required_error: 'Please enter your username or email' }).email(),
+  password: passwordSchema,
+});
+
+type LoginFormValues = z.infer<typeof loginFormSchema>;
+
+export default function Login() {
+  const { toast } = useToast();
+  const router = useRouter();
+  const [isPasswordHidden, setPasswordHidden] = useState(true);
+  const [signInWithEmailAndPassword, , loading] = useSignInWithEmailAndPassword(auth);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+    }
+  };
+
+  const defaultValues: Partial<LoginFormValues> = {
+    email: '',
+    password: '',
+  };
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues,
+  });
+
+  const handleLogin = async (inputs: LoginInputs) => {
+    try {
+      const newUser = await signInWithEmailAndPassword(inputs.email, inputs.password);
+      if (newUser) {
+        router.push('/session');
+      }
+    } catch (err: any) {
+      toast({
+        variant: 'destructive',
+        title: 'Invalid credentials',
+        description: err.message,
+      });
+    }
+  };
+
+  return (
+    <main className="w-full h-screen flex flex-col items-center justify-center sm:px-4">
+      <div className="w-full space-y-6 text-gray-400 sm:max-w-md">
+        <div className="text-center">
+          <div className="mt-5 space-y-2">
+            <h3 className="text-white text-2xl font-bold sm:text-3xl">Log in to your account</h3>
+            <p className="">
+              Don&apos;t have an account?{' '}
+              <Link
+                href="/auth/signup/"
+                className="font-medium text-brand-purple-s hover:text-brand-purple">
+                Sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="bg-neutral-800 shadow p-4 py-6 sm:p-6 sm:rounded-lg">
+          <Form {...form}>
+            <form className="space-y-8 text-[#f5f5f5]" onSubmit={form.handleSubmit(handleLogin)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="johndoe@john.doe"
+                        autoComplete="email"
+                        {...field}
+                        onKeyDown={handleKeyDown}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-white">Password</FormLabel>
+                    <FormControl>
+                      <div className="relative mt-2">
+                        <button
+                          type="button"
+                          className="text-gray-400 absolute right-3 inset-y-0 my-auto active:text-gray-600"
+                          onClick={() => setPasswordHidden(!isPasswordHidden)}>
+                          {isPasswordHidden ? <Icons.eye /> : <Icons.eyesplash />}
+                        </button>
+                        <Input
+                          placeholder="********"
+                          type={isPasswordHidden ? 'password' : 'text'}
+                          autoComplete="current-password"
+                          {...field}
+                          onKeyDown={handleKeyDown}
+                        />
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <div className="flex items-center justify-end text-sm">
+                <Link
+                  href="/auth/password/forgot-password"
+                  className="text-center text-brand-purple-s hover:text-brand-purple">
+                  Forgot password?
+                </Link>
+              </div>
+
+              <Button
+                type="submit"
+                className="w-full px-4 text-white font-medium bg-brand-purple-s hover:bg-brand-purple rounded-lg duration-150">
+                {loading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
+                Log In
+              </Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -37,3 +37,16 @@ export type SiteConfig = {
     authorSite: string;
   };
 };
+
+export type SignupInputs = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+export type LoginInputs = {
+  email: string;
+  password: string;
+};
