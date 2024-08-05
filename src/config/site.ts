0e492bd791c6e2c55380d5e9614b48e8c8afdf00
Abass Hammed <abasshammedola@gmail.com>
2024-08-05 13:13:02 +0200

    Still setting up

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import type { SiteConfig } from '@types';
+
+export const siteConfig: SiteConfig = {
+  name: 'LetsCode Online IDE',
+  description: 'Online compiler for all sorts of languages.',
+  url: 'https://letscode.vercel.app',
+  ogImage: 'https://letscode.vercel.app/ogImage.png',
+  links: {
+    github: 'https://github.com/AbassHammed/LetsCode',
+    githubUsername: 'AbassHammed',
+    authorSite: 'https://hammedabass.com',
+  },
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -25,3 +25,15 @@ export type UseLocalStorageOptions<T> = {
   deserializer?: (value: string) => T;
   initializeWithValue?: boolean;
 };
+
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    github: string;
+    githubUsername: string;
+    authorSite: string;
+  };
+};
