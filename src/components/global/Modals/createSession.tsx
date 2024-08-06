15de2d8842b1aea8bc46b9accdf3b55fc937a9c3
Abass Hammed <abasshammedola@gmail.com>
2024-08-06 16:02:15 +0200

    What would Ye have said ?

--- src/components/global/Modals/createSession.tsx
+++ src/components/global/Modals/createSession.tsx
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 'use client';
 
 import { useState } from 'react';
 
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
 import { firestore, storage } from '@/firebase/firebase';
@@ -12,7 +10,6 @@ import {
   Button,
   Dialog,
   DialogContent,
-  DialogFooter,
   DialogTitle,
   DialogTrigger,
   Form,
@@ -50,15 +47,17 @@ export default function CreateSession() {
   const [open, setOpen] = useState(false);
   const [pdfFile, setPdfFile] = useState<File | null>();
   const [progress, setProgress] = useState(0);
-  const [sessionId, setSessionId] = useState('');
   const [loading, setLoading] = useState(false);
-  const [showFile, setShowFile] = useState(false);
+  const [showFile, setShowFile] = useState(true);
   const { user } = useAuth();
   const router = useRouter();
 
-  const generateSessionId = () => Math.random().toString(36).slice(-16).toUpperCase();
-
-  const handleGenerateSessionId = () => setSessionId(generateSessionId());
+  const generateSessionId = () => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    return Array.from({ length: 16 }, () => chars[Math.floor(Math.random() * chars.length)]).join(
+      '',
+    );
+  };
 
   const handleSelectedFile = (files: FileList | null) => {
     if (!files || files.length === 0) {
@@ -126,7 +125,11 @@ export default function CreateSession() {
     }
 
     try {
-      const filePath = await handleUploadFile();
+      let filePath = null;
+
+      if (showFile) {
+        filePath = await handleUploadFile();
+      }
       await addDoc(collection(firestore, 'sessions'), {
         userId: user.uid,
         sessionName: data.sessionName,
@@ -156,13 +159,15 @@ export default function CreateSession() {
   const defaultValues: Partial<CreateSessionValues> = {
     sessionName: '',
     sessionId: '',
-    showPdf: false,
+    showPdf: true,
   };
 
   const form = useForm<CreateSessionValues>({
     resolver: zodResolver(createSessionSchema),
     defaultValues,
   });
+
+  const handleGenerateSessionId = () => form.setValue('sessionId', generateSessionId());
 
   const handleRemoveFile = () => setPdfFile(null);
 
@@ -221,7 +226,6 @@ export default function CreateSession() {
                           type="text"
                           autoComplete="sessionId"
                           {...field}
-                          value={sessionId}
                           onKeyDown={handleKeyDown}
                         />
                       </div>
@@ -236,7 +240,7 @@ export default function CreateSession() {
                   control={form.control}
                   name="showPdf"
                   render={({ field }) => (
-                    <FormItem className="flex flex-row items-center justify-between rounded-lg border border-white border-opacity-50 p-3 shadow-sm">
+                    <FormItem className="flex flex-row items-center justify-between rounded-lg border border-gray-200 border-opacity-50 p-3 shadow-sm">
                       <div className="space-y-0.5">
                         <FormLabel>Include PDF file</FormLabel>
                         <FormDescription>
@@ -257,61 +261,53 @@ export default function CreateSession() {
                 />
               </div>
 
-              <div className=" mx-auto mt-5">
-                <div className="max-w-lg mx-auto">
-                  <div className="max-w-md h-40 rounded-lg border-2 border-gray-200 border-dashed flex items-center justify-center">
-                    <label htmlFor="file" className="cursor-pointer text-center p-4 md:p-8">
-                      <svg
-                        className="w-10 h-10 mx-auto"
-                        viewBox="0 0 41 40"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path
-                          d="M12.1667 26.6667C8.48477 26.6667 5.5 23.6819 5.5 20C5.5 16.8216 7.72428 14.1627 10.7012 13.4949C10.5695 12.9066 10.5 12.2947 10.5 11.6667C10.5 7.0643 14.231 3.33334 18.8333 3.33334C22.8655 3.33334 26.2288 6.19709 27.0003 10.0016C27.0556 10.0006 27.1111 10 27.1667 10C31.769 10 35.5 13.731 35.5 18.3333C35.5 22.3649 32.6371 25.7279 28.8333 26.5M25.5 21.6667L20.5 16.6667M20.5 16.6667L15.5 21.6667M20.5 16.6667L20.5 36.6667"
-                          stroke="#4F46E5"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+              {showFile && (
+                <div className=" mx-auto mt-5">
+                  <div className="max-w-lg mx-auto">
+                    {!pdfFile && (
+                      <div className="max-w-md h-40 rounded-lg border border-gray-200 border-dashed border-opacity-50 flex items-center justify-center">
+                        <label htmlFor="file" className="cursor-pointer text-center p-4 md:p-8">
+                          <Icons.upload />
+                          <p className="mt-3 text-white max-w-xs mx-auto">
+                            Click to{' '}
+                            <span className="font-medium text-indigo-600">Upload your file</span>
+                          </p>
+                        </label>
+                        <input
+                          id="file"
+                          type="file"
+                          className="hidden"
+                          accept="application/pdf"
+                          onChange={e => handleSelectedFile(e.target.files)}
                         />
-                      </svg>
-                      <p className="mt-3 text-white max-w-xs mx-auto">
-                        Click to{' '}
-                        <span className="font-medium text-indigo-600">Upload your file</span> or
-                        drag and drop your file here
-                      </p>
-                    </label>
-                    <input
-                      id="file"
-                      type="file"
-                      className="hidden"
-                      accept="application/pdf"
-                      onChange={e => handleSelectedFile(e.target.files)}
-                    />
-                  </div>
-
-                  {pdfFile && (
-                    <div className="mt-5 bg-white p-4 rounded shadow relative">
-                      <button
-                        className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
-                        onClick={handleRemoveFile}>
-                        <Cross2Icon className="h-4 w-4" />
-                      </button>
-
-                      <p className="text-lg text-gray-500 font-semibold">{pdfFile.name}</p>
-                      <p className="text-sm text-gray-500">Size: {pdfFile.size} bytes</p>
-
-                      <div className="mt-3 bg-gray-200 rounded h-2.5 dark:bg-gray-700">
-                        <div
-                          className="bg-blue-600 h-2.5 rounded"
-                          style={{ width: `${progress}%` }}></div>
                       </div>
-                    </div>
-                  )}
+                    )}
+
+                    {pdfFile && (
+                      <div className="mt-5 bg-white p-4 rounded shadow relative">
+                        <button
+                          className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
+                          onClick={handleRemoveFile}>
+                          <Cross2Icon className="h-4 w-4" />
+                        </button>
+
+                        <p className="text-lg text-gray-500 font-semibold">{pdfFile.name}</p>
+                        <p className="text-sm text-gray-500">Size: {pdfFile.size} bytes</p>
+
+                        <div className="mt-3 bg-gray-200 rounded h-2.5 dark:bg-gray-700">
+                          <div
+                            className="bg-blue-600 h-2.5 rounded"
+                            style={{ width: `${progress}%` }}></div>
+                        </div>
+                      </div>
+                    )}
+                  </div>
                 </div>
-              </div>
+              )}
 
               <Button
                 type="submit"
+                disabled={showFile && !pdfFile}
                 className="w-full px-4 text-white font-medium bg-brand-purple hover:bg-brand-purple-s rounded-lg duration-150">
                 {loading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}
                 {loading ? 'Creating ...' : 'Create'}
