1a47d178f58951cfad7e30eb34eea43836936b93
Abass Hammed <abasshammedola@gmail.com>
2024-08-06 19:45:46 +0200

    Working ...

--- src/components/global/Topbar/themes.tsx
+++ src/components/global/Topbar/themes.tsx
@@ -3,6 +3,14 @@
 
 import { useState } from 'react';
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuPortal,
+  DropdownMenuTrigger,
+  Icons,
+} from '@components';
+import { Themes } from '@config/constants';
 import { useTernaryDarkMode } from '@hooks';
 import { useTheme } from 'next-themes';
 
@@ -17,5 +25,43 @@ export default function Apparence() {
     setTernaryDarkMode(theme);
   };
 
-  return <div></div>;
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded cursor-pointer flex flex-row items-center justify-between w-full py-3 space-x-6 px-2 md:space-x-3 md:py-[10px] dark:hover:bg-[#404040] hover:bg-[#f5f5f5]">
+          <div className="leading-none">
+            {selectedKey === 'dark' ? (
+              <Icons.moon />
+            ) : selectedKey === 'light' ? (
+              <Icons.sun />
+            ) : (
+              <Icons.sunmoon />
+            )}
+          </div>
+          <div className="grow text-left"> Apparence</div>
+          <Icons.chevronRight className="w-[14px] h-[14px]" />
+        </DropdownMenuTrigger>
+        <DropdownMenuPortal>
+          <DropdownMenuContent className="w-48 dark:bg-[#303030] border-none" side="left">
+            <div className="flex flex-col">
+              {Themes.map((theme, idx) => (
+                <div
+                  onClick={() => handleThemeChange(theme.value as 'dark' | 'light' | 'system')}
+                  key={idx}
+                  className="relative flex w-full p-1 m-1 rounded-[4px] dark:text-[#f5f5f5] dark:hover:bg-[#404040] hover:bg-[#f5f5f5] focus:outline-none cursor-pointer">
+                  <span
+                    className={`flex items-center mr-2 ${
+                      selectedKey === theme.value ? 'visible' : 'invisible'
+                    }`}>
+                    <Icons.check />
+                  </span>
+                  <div className="text-left text-[14px]">{theme.name}</div>
+                </div>
+              ))}
+            </div>
+          </DropdownMenuContent>
+        </DropdownMenuPortal>
+      </DropdownMenu>
+    </div>
+  );
 }
--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -284,6 +284,113 @@ const Icons = {
       />
     </svg>
   ),
+  sun: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <circle cx="12" cy="12" r="4" />
+      <path d="M12 2v2" />
+      <path d="M12 20v2" />
+      <path d="m4.93 4.93 1.41 1.41" />
+      <path d="m17.66 17.66 1.41 1.41" />
+      <path d="M2 12h2" />
+      <path d="M20 12h2" />
+      <path d="m6.34 17.66-1.41 1.41" />
+      <path d="m19.07 4.93-1.41 1.41" />
+    </svg>
+  ),
+  moon: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
+    </svg>
+  ),
+  sunmoon: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <path d="M12 8a2.83 2.83 0 0 0 4 4 4 4 0 1 1-4-4" />
+      <path d="M12 2v2" />
+      <path d="M12 20v2" />
+      <path d="m4.9 4.9 1.4 1.4" />
+      <path d="m17.7 17.7 1.4 1.4" />
+      <path d="M2 12h2" />
+      <path d="M20 12h2" />
+      <path d="m6.3 17.7-1.4 1.4" />
+      <path d="m19.1 4.9-1.4 1.4" />
+    </svg>
+  ),
+  chevronRight: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <path d="m9 18 6-6-6-6" />
+    </svg>
+  ),
+  quit: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <circle cx="12" cy="12" r="10" />
+      <path d="m4.9 4.9 14.2 14.2" />
+    </svg>
+  ),
+  check: (props: IconProps) => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}>
+      <path d="M20 6 9 17l-5-5" />
+    </svg>
+  ),
 };
 
 export default Icons;
