92d5b28fc864a9f66d127e288a9c66fb59713d30
Abass Hammed <abasshammedola@gmail.com>
2024-08-06 14:07:17 +0200

    well the create session modal needs a lot of time

--- src/components/global/Modals/createSession.tsx
+++ src/components/global/Modals/createSession.tsx
@@ -28,6 +28,7 @@ import {
 } from '@components';
 import { zodResolver } from '@hookform/resolvers/zod';
 import Icons from '@icons';
+import { Cross2Icon } from '@radix-ui/react-icons';
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
 import { useForm } from 'react-hook-form';
@@ -254,6 +255,59 @@ export default function CreateSession() {
                     </FormItem>
                   )}
                 />
+              </div>
+
+              <div className=" mx-auto mt-5">
+                <div className="max-w-lg mx-auto">
+                  <div className="max-w-md h-40 rounded-lg border-2 border-gray-200 border-dashed flex items-center justify-center">
+                    <label htmlFor="file" className="cursor-pointer text-center p-4 md:p-8">
+                      <svg
+                        className="w-10 h-10 mx-auto"
+                        viewBox="0 0 41 40"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M12.1667 26.6667C8.48477 26.6667 5.5 23.6819 5.5 20C5.5 16.8216 7.72428 14.1627 10.7012 13.4949C10.5695 12.9066 10.5 12.2947 10.5 11.6667C10.5 7.0643 14.231 3.33334 18.8333 3.33334C22.8655 3.33334 26.2288 6.19709 27.0003 10.0016C27.0556 10.0006 27.1111 10 27.1667 10C31.769 10 35.5 13.731 35.5 18.3333C35.5 22.3649 32.6371 25.7279 28.8333 26.5M25.5 21.6667L20.5 16.6667M20.5 16.6667L15.5 21.6667M20.5 16.6667L20.5 36.6667"
+                          stroke="#4F46E5"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                        />
+                      </svg>
+                      <p className="mt-3 text-white max-w-xs mx-auto">
+                        Click to{' '}
+                        <span className="font-medium text-indigo-600">Upload your file</span> or
+                        drag and drop your file here
+                      </p>
+                    </label>
+                    <input
+                      id="file"
+                      type="file"
+                      className="hidden"
+                      accept="application/pdf"
+                      onChange={e => handleSelectedFile(e.target.files)}
+                    />
+                  </div>
+
+                  {pdfFile && (
+                    <div className="mt-5 bg-white p-4 rounded shadow relative">
+                      <button
+                        className="absolute top-2 right-2 text-gray-400 hover:text-gray-600"
+                        onClick={handleRemoveFile}>
+                        <Cross2Icon className="h-4 w-4" />
+                      </button>
+
+                      <p className="text-lg text-gray-500 font-semibold">{pdfFile.name}</p>
+                      <p className="text-sm text-gray-500">Size: {pdfFile.size} bytes</p>
+
+                      <div className="mt-3 bg-gray-200 rounded h-2.5 dark:bg-gray-700">
+                        <div
+                          className="bg-blue-600 h-2.5 rounded"
+                          style={{ width: `${progress}%` }}></div>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
 
               <Button
