5bfdd5fb36623b7805622abab4a8e23956316678
Abass Hammed <abasshammedola@gmail.com>
2024-08-10 19:57:20 +0200

    implmenting real data for the table

--- src/components/global/Dashboard/dashtable.tsx
+++ src/components/global/Dashboard/dashtable.tsx
@@ -1,26 +1,102 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+'use client';
 
-import { z } from 'zod';
+// import { promises as fs } from 'fs';
+// import path from 'path';
+import { useEffect, useState } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import Loading from '@components/shared/loading';
+import { toast } from '@components/shared/toast';
+import { firestore } from '@firebase/firebase';
+import { useAuth } from '@hooks';
+import { collection, onSnapshot, query, Timestamp, where } from 'firebase/firestore';
+
+// import { z } from 'zod';
 
 import { columns } from './columns';
-import { userSchema } from './data/schema';
+import { UserType } from './data/schema';
 import { DataTable } from './table';
 
 // Simulate a database read for tasks.
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), 'src/components/global/Dashboard/data/users.json'),
-    'utf8',
-  );
+// async function getTasks() {
+//   const data = await fs.readFile(
+//     path.join(process.cwd(), 'src/components/global/Dashboard/data/users.json'),
+//     'utf8',
+//   );
 
-  const tasks = JSON.parse(data.toString());
+//   const tasks = JSON.parse(data.toString());
 
-  return z.array(userSchema).parse(tasks);
-}
+//   return z.array(userSchema).parse(tasks);
+// }
 
-export default async function TaskPage() {
-  const tasks = await getTasks();
+export default function TaskPage() {
+  // const tasks = await getTasks();
+  const { user, loading } = useAuth();
+  const [users, setUsers] = useState<UserType[]>([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      return;
+    }
+
+    const sessionQuery = query(collection(firestore, 'sessions'), where('userId', '==', user.uid));
+    const unsubscribeSession = onSnapshot(sessionQuery, querySnapshot => {
+      if (querySnapshot.empty) {
+        toast({
+          variant: 'destructive',
+          title: 'No session',
+          description: 'It seems like you do not have any open sessions',
+        });
+        router.push('/session');
+        return;
+      }
+
+      const sessionDoc = querySnapshot.docs[0];
+      const usersRef = collection(firestore, 'sessions', sessionDoc.id, 'users');
+      onSnapshot(usersRef, async snapshot => {
+        const usersDataPromises = snapshot.docs.map(async doc => {
+          const basicUserData: {
+            name: string;
+            connected: boolean;
+            joinedAt: Timestamp;
+            quittedAt: Timestamp;
+          } = doc.data() as {
+            name: string;
+            connected: boolean;
+            joinedAt: Timestamp;
+            quittedAt: Timestamp;
+          };
+
+          return {
+            id: doc.id,
+            name: basicUserData.name,
+            connected: basicUserData.connected ? 'connected' : 'disconnected',
+            joinedAt: basicUserData.joinedAt.toDate().toLocaleTimeString() as string,
+            quittedAt: basicUserData.quittedAt
+              ? (basicUserData.quittedAt.toDate().toLocaleTimeString() as string)
+              : null,
+          };
+        });
+
+        const combinedUsersData = (await Promise.all(usersDataPromises)).filter(
+          user => user !== undefined,
+        ) as UserType[];
+        setUsers(combinedUsersData);
+      });
+    });
+
+    return () => {
+      if (unsubscribeSession) {
+        unsubscribeSession();
+      }
+    };
+  }, [user, router]);
+
+  if (loading) {
+    <Loading />;
+  }
 
   return (
     <div className="h-full flex-1 flex-col space-y-8 p-8 flex">
@@ -31,7 +107,7 @@ export default async function TaskPage() {
         </div>
       </div>
       <div className="overflow-auto">
-        <DataTable data={tasks} columns={columns} />
+        <DataTable data={users} columns={columns} />
       </div>
     </div>
   );
