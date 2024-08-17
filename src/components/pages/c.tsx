39c51f2475816e0d796a36653a1886dc0d2235bb
Abass Hammed <abasshammedola@gmail.com>
2024-08-17 02:43:02 +0200

    Adding mouseleave, mouseenter and visibilitychange eventhandler

--- src/components/pages/c.tsx
+++ src/components/pages/c.tsx
@@ -2,22 +2,21 @@
 
 import React, { useEffect, useState } from 'react';
 
+import { useRouter } from 'next/navigation';
+
 import { useAuth } from '@/hooks/useAuth';
 import { useSession } from '@/hooks/useSession';
-// import { useRouter } from 'next/router';
-
 import { Loading, Topbar } from '@components';
 import Workspace from '@components/global/Workspace';
 import { firestore } from '@firebase/firebase';
-import { doc, onSnapshot } from 'firebase/firestore';
-
-// import { doc, onSnapshot, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { doc, onSnapshot, serverTimestamp, updateDoc } from 'firebase/firestore';
 
 const Compiler: React.FC = () => {
   const { sessionData } = useSession();
   const { user, loading: authLoading } = useAuth();
   const [showScreen, setShowScreen] = useState(false);
-  // const router = useRouter();
+  const [showOverlay, setShowOverlay] = useState(true);
+  const router = useRouter();
 
   useEffect(() => {
     if (!user || !sessionData) {
@@ -33,45 +32,62 @@ const Compiler: React.FC = () => {
       },
     );
 
-    return () => unsubscribe();
+    document.addEventListener('mouseleave', () => {
+      setShowOverlay(true);
+    });
+
+    document.addEventListener('mouseenter', () => {
+      setShowOverlay(false);
+    });
+
+    return () => {
+      unsubscribe();
+      document.removeEventListener('mouseleave', () => {
+        setShowOverlay(true);
+      });
+
+      document.removeEventListener('mouseenter', () => {
+        setShowOverlay(false);
+      });
+    };
   }, [user, sessionData]);
 
-  // useEffect(() => {
-  //   const handleDisconnect = async () => {
-  //     if (
-  //       user &&
-  //       typeof sessionData?.sessionDocId === 'string' &&
-  //       document.visibilityState === 'hidden'
-  //     ) {
-  //       const userRef = doc(firestore, `sessions/${sessionData.sessionDocId}/users`, user.uid);
-  //       await updateDoc(userRef, {
-  //         connected: false,
-  //         quitedAt: serverTimestamp(),
-  //       });
-  //     }
-  //   };
+  useEffect(() => {
+    const handleDisconnect = async () => {
+      if (
+        user &&
+        typeof sessionData?.sessionDocId === 'string' &&
+        document.visibilityState === 'hidden'
+      ) {
+        const userRef = doc(firestore, `sessions/${sessionData.sessionDocId}/users`, user.uid);
+        await updateDoc(userRef, {
+          connected: false,
+          quitedAt: serverTimestamp(),
+        });
+      }
+    };
 
-  //   if (!sessionData || !user) {
-  //     return;
-  //   }
+    if (!sessionData || !user) {
+      return;
+    }
 
-  //   const unsubscribe = onSnapshot(
-  //     doc(firestore, `sessions/${sessionData.sessionDocId}/users`, user.uid),
-  //     docSnapshot => {
-  //       if (!docSnapshot.exists() || !docSnapshot.data()?.connected) {
-  //         router.push('/session');
-  //       }
-  //     },
-  //   );
+    const unsubscribe = onSnapshot(
+      doc(firestore, `sessions/${sessionData.sessionDocId}/users`, user.uid),
+      docSnapshot => {
+        if (!docSnapshot.exists() || !docSnapshot.data()?.connected) {
+          router.push('/session');
+        }
+      },
+    );
 
-  //   // document.addEventListener('visibilitychange', handleDisconnect);
+    document.addEventListener('visibilitychange', handleDisconnect);
 
-  //   return () => {
-  //     // document.removeEventListener('visibilitychange', handleDisconnect);
+    return () => {
+      document.removeEventListener('visibilitychange', handleDisconnect);
 
-  //     unsubscribe();
-  //   };
-  // }, [user, authLoading, sessionData, router]);
+      unsubscribe();
+    };
+  }, [user, authLoading, sessionData, router]);
 
   if (!user || authLoading || !sessionData) {
     return <Loading />;
@@ -79,6 +95,15 @@ const Compiler: React.FC = () => {
 
   return showScreen ? (
     <div className="!min-h-full w-full">
+      <div
+        className={`fixed inset-0 h-full w-full bg-black bg-opacity-80 z-50 ${showOverlay ? 'visible' : 'invisible'} `}
+        onClick={() => setShowOverlay(false)}>
+        <div className="flex h-full items-center justify-center">
+          <span className="text-white fonth1 font-medium text-2xl">
+            Leaving this page will automatically disconnect you from the session
+          </span>
+        </div>
+      </div>
       <Topbar compilerPage={true} dashboardPage={false} />
       <Workspace />
     </div>
