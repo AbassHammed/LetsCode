7f9ef04dd340f1455488eec52b664417a782cb66
Abass Hammed <abasshammedola@gmail.com>
2024-08-18 22:01:29 +0200

    Added a not-found component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,20 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/navigation';
+
+import { toast } from '@components';
+
+export default function NotFound() {
+  const { push } = useRouter();
+  useEffect(() => {
+    toast({
+      variant: 'destructive',
+      title: 'Page not found',
+      description: 'The page you are trying to access does not exist',
+    });
+    push('/session');
+  }, []);
+}
